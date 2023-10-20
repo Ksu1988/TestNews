9d65ed4b97d9e3f7ece936cf7f232e3c4d2b8aa5
Kseniia Chukhareva <SC12203@stada.ru>
2023-10-20 12:36:23 +0300

    Add project files.

---
+++ News.cs
@@ -0,0 +1,54 @@
+﻿using NewsAPI;
+using NewsAPI.Constants;
+using NewsAPI.Models;
+using System.Text.RegularExpressions;
+
+namespace TestNews
+{
+    internal class News
+    {
+        internal ArticlesResult GetArticles(string q, SortBys sortBys, Languages lang, DateTime dateTime)
+        {
+            var apiKey = System.Configuration.ConfigurationManager.AppSettings["key"];
+            var newsApiClient = new NewsApiClient(apiKey);
+            var articlesResponse = newsApiClient.GetEverything(new EverythingRequest
+            {
+                Q = q,
+                SortBy = sortBys,
+                Language = lang,
+                From = dateTime
+            });
+            var pattern =  new Regex("[^aeiouy]+", RegexOptions.IgnoreCase);
+            if (lang== Languages.RU)
+            {
+                pattern = new Regex("[^уеёыаоэяию]+", RegexOptions.IgnoreCase);                
+            }
+            if (articlesResponse.Status == Statuses.Ok)
+            {
+                foreach (var article in articlesResponse.Articles)
+                {
+                    var words = Regex.Matches(article.Description, @"\w+").ToList();
+                    var maxWord = "";
+                    var maxVowelsCount = 0;
+                    foreach (var word in words)
+                    {
+                        var vowelsMatches = pattern.Matches(word.Value);
+                        if (vowelsMatches.Count() > maxVowelsCount)
+                        {
+                            maxWord = word.Value;
+                            maxVowelsCount = vowelsMatches.Count();
+                        }
+                    }
+                    Console.WriteLine($"{article.Description} - {maxWord}");
+                }
+            }
+            else
+            {
+                Console.WriteLine(articlesResponse.Error);
+            }
+            return articlesResponse;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+//Your API key is: 2f1fef8150be40c9aedfa9303c561f31
+using NewsAPI.Constants;
+using TestNews;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+var newsApi = new News();
+newsApi.GetArticles("Космос", SortBys.Popularity, Languages.RU, new DateTime(2023, 09, 25));
+
+
+
+
